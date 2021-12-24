349103590b82ed082b641fe5c1df69647ad0b174
Savchenko Roman <r.savchenko@corp.mail.ru>
2021-12-24 20:22:05 +0300

    update terrafrom version

--- cloud-provision-mcs/postgres/main.tf
+++ cloud-provision-mcs/postgres/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.14"
+
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
 # Create network port for postgres node
 resource "openstack_networking_port_v2" "postgres_port" {
   count          = var.postgres_enable
